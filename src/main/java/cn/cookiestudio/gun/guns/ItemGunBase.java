a5f87659b36a8202595c500ab701f1556bb86f57
smart_cmd <3523206925@qq.com>
2022-02-01 21:22:04 +0800

    something

--- src/main/java/cn/cookiestudio/gun/guns/ItemGunBase.java
+++ src/main/java/cn/cookiestudio/gun/guns/ItemGunBase.java
@@ -93,7 +93,7 @@ public abstract class ItemGunBase extends ItemEdible {
     @Override
     public boolean onClickAir(Player player, Vector3 directionVector) {
         GunInteractAction action = this.interact(player);
-        return true;
+        return false;
     }
 
     @Override
